0e726bca5006dd6f93749dad63f72ce36c20e4f6
Roman Shishkin <sparklingfire@yandex.ru>
2015-06-26 10:05:19 +0200

    Rework 503.php - session already started, fix doctype to html5

--- 503.php
+++ 503.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 header("HTTP/1.0 503 Service Temporarily Unavailable");
 header("Content-type: text/html; charset=UTF-8");
 header("Expires: Mon, 23 May 1995 02:00:00 GMT");
@@ -9,42 +8,25 @@ header("Pragma: no-cache;");
 $base = "http://".$_SERVER['HTTP_HOST'].(dirname($_SERVER['PHP_SELF']) != "\\" ? dirname($_SERVER['PHP_SELF']) : "");
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="Autor" content="Королёв Алексей a.k.a _emploi-kun_ (программирование) +79022511929">
+        <meta name="robots" content="index,follow">
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <title>soft 503 — Сервис временно недоступен</title>
+    </head>
+    <body>
+        <div style='text-align: center; font-family: Verdana; font-size: 22px; color: #363640; width: 1024px; margin: 0px auto; outline-style: none;'>
+            <h1>Киоск не работает</h1>
 
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta name="Autor" content="Королёв Алексей a.k.a _emploi-kun_ (программирование) +79022511929">
-<meta name="robots" content="index,follow">
+            <img style="width:303px" src="<?php echo $base;?>/files/images/_system/error.png">
 
-<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-<link rel="icon" href="/favicon.ico" type="image/x-icon" />
-
-
-<title>soft 503 — Сервис временно недоступен</title>
-
-</head>
-
-<body >
-
-<div  style='text-align: center; font-family: Verdana; font-size: 22px; color: #363640; width: 1024px; margin: 0px auto; outline-style: none;'>
- <H1>
-  Киоск не работает
- </H1>
-
- <img style="width:303px" src="<?php echo $base;?>/files/images/_system/error.png">
-
- <H2>
-   Отказ Базы Данных
- </H2>
- <H3>
-   Вызовите мастера +7(902)251-19-29
- </H3>
- <H3>
-   <a href="<?php echo $base;?>">Обновить</a>
- </H3>
-
-</div>
-
-</body>
+            <h2>Отказ базы данных</h2>
+            <h3>Вызовите мастера +7(902)251-19-29</h3>
+            <h3><a href="<?php echo $base;?>">Обновить</a></h3>
+        </div>
+    </body>
 </html>
